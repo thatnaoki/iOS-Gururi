97a34183d64e6b341b228a88466237e93c6f7729
(thatnaoki) <muroya.naoki@gmail.com>
2019-02-19 14:11:53 +0900

    invitecontrollerのpickerviewおよびtextfieldのvalidation周りの実装

--- Gururi/InviteViewController.swift
+++ Gururi/InviteViewController.swift
@@ -11,48 +11,144 @@ import UIKit
 class InviteViewController: UIViewController {
     
     // properties
+    @IBOutlet weak var shopNameTextField: UITextField!
     @IBOutlet weak var dateTextField: UITextField!
-    @IBOutlet weak var timeTextField: UITextField!
+    @IBOutlet weak var timeTextField: PickerTextField!
+    @IBOutlet weak var guestNameTextField: UITextField!
+    @IBOutlet weak var peopleTextField: PickerTextField!
+    @IBOutlet weak var telTextField: UITextField!
     @IBOutlet weak var addButton: UIButton!
     
-    // prepare date and time picker
-    let datePicker = UIDatePicker()
+    // prepare lists for picker view
+    var timeList : [String] = ["18:00", "19:00", "20:00", "21:00"]
+    var peopleList : [String] = ["2", "3", "4", "5", "6", "7"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // button layout
-        addButton.backgroundColor = UIColor(red: 17/255, green: 154/255, blue: 237/255, alpha: 1)
+        addButton.isEnabled = false
+        addButton.backgroundColor = UIColor(red: 149/255, green: 204/255, blue: 244/255, alpha: 1)
         addButton.layer.cornerRadius = 20.0
         
+        // validation check
+        shopNameTextField.addTarget(self, action: #selector(formValidation), for: .editingChanged)
+        dateTextField.addTarget(self, action: #selector(formValidation), for: .editingChanged)
+        timeTextField.addTarget(self, action: #selector(formValidation), for: .editingChanged)
+        guestNameTextField.addTarget(self, action: #selector(formValidation), for: .editingChanged)
+        peopleTextField.addTarget(self, action: #selector(formValidation), for: .editingChanged)
+        telTextField.addTarget(self, action: #selector(formValidation), for: .editingChanged)
+        
+        // prepare date picker views
         prepareDatePicker()
+        // prepare time picker views
+        timeTextField.setup(dataList: timeList)
+        // prepare people picker view
+        peopleTextField.setup(dataList: peopleList)
 
     }
     
     // functions
     func prepareDatePicker() {
         
+        let datePicker = UIDatePicker()
         datePicker.datePickerMode = UIDatePicker.Mode.date
         datePicker.timeZone = NSTimeZone.local
         datePicker.locale = Locale.current
         
         let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 35))
-        let spacelItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
-        let doneItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
-        toolbar.setItems([spacelItem, doneItem], animated: true)
+        let spaceItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let doneItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneForDatePicker))
+        toolbar.setItems([spaceItem, doneItem], animated: true)
         
         dateTextField.inputView = datePicker
         dateTextField.inputAccessoryView = toolbar
         
     }
     
-    @objc func done() {
+    // func called when item choosen
+    @objc func doneForDatePicker() {
         dateTextField.endEditing(true)
         
-        // 日付のフォーマット
+        // date format
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
         dateTextField.text = "\(formatter.string(from: Date()))"
     }
+    
+    // close keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        shopNameTextField.resignFirstResponder()
+        guestNameTextField.resignFirstResponder()
+        telTextField.resignFirstResponder()
+        return true
+        
+    }
+    
+    @objc func formValidation() {
+        
+        guard
+            shopNameTextField.hasText,
+            dateTextField.hasText,
+            timeTextField.hasText,
+            guestNameTextField.hasText,
+            peopleTextField.hasText,
+            telTextField.hasText else {
+                // handle case for above conditions not met
+                addButton.isEnabled = false
+                addButton.backgroundColor = UIColor(red: 149/255, green: 204/255, blue: 244/255, alpha: 1)
+                return
+        }
+        
+        // handle case for conditions were met
+        addButton.isEnabled = true
+        addButton.backgroundColor = UIColor(red: 17/255, green: 154/255, blue: 237/255, alpha: 1)
+        
+    }
 
+}
+
+
+
+// extension for time picker
+extension InviteViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func prepareTimePicker() {
+        
+        let timePicker = UIPickerView()
+        timePicker.delegate = self
+        timePicker.dataSource = self
+        timePicker.showsSelectionIndicator = true
+        
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 35))
+        let spaceItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let doneItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneForTimePicker))
+        toolbar.setItems([spaceItem, doneItem], animated: true)
+        
+        timeTextField.inputView = timePicker
+        timeTextField.inputAccessoryView = toolbar
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return timeList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return timeList[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        timeTextField.text = timeList[row]
+    }
+    
+    @objc func doneForTimePicker() {
+        timeTextField.endEditing(true)
+    }
+    
 }
---
+++ Gururi/PickerTextField.swift
@@ -0,0 +1,65 @@
+//
+//  PickerTextField.swift
+//  Gururi
+//
+//  Created by Naoki Muroya on 2019/02/19.
+//  Copyright © 2019 Gururi. All rights reserved.
+//
+
+import UIKit
+
+class PickerTextField: UITextField, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    var dataList = [String]()
+    
+//    init() {
+//        super.init(CGRect.zero)
+//    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func setup(dataList: [String]){
+        self.dataList = dataList
+        
+        let picker = UIPickerView()
+        picker.delegate = self
+        picker.dataSource = self
+        picker.showsSelectionIndicator = true
+        
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 0, height: 35))
+        let spaceItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let doneItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneForTimePicker))
+        toolbar.setItems([spaceItem, doneItem], animated: true)
+        
+        self.inputView = picker
+        self.inputAccessoryView = toolbar
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dataList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return dataList[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.text = dataList[row]
+    }
+    
+    @objc func doneForTimePicker() {
+        self.endEditing(true)
+    }
+
+}
